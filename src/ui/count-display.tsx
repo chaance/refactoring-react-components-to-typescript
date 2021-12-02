042bb34f3e4915c0f2d21984cd3d20b6c7356ba3
Chance Strickland <hi@chance.dev>
2021-12-02 07:04:16 -0800

    sync w/ lesson 6

--- src/ui/count-display.tsx
+++ src/ui/count-display.tsx
@@ -2,11 +2,18 @@ import * as React from "react";
 import cx from "clsx";
 import { scope } from "../lib/utils";
 
-function CountDisplay({ count, className }) {
+const CountDisplay: React.VFC<CountDisplayProps> = ({ count, className }) => {
 	let countString = String(Math.max(Math.min(count, 999), -99));
 	return (
 		<div className={cx(scope("count-display"), className)}>{countString}</div>
 	);
-}
+};
+
+CountDisplay.displayName = "CountDisplay";
 
 export { CountDisplay };
+
+interface CountDisplayProps {
+	count: number;
+	className?: string;
+}
